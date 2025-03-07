3503f9fe6b88be591349e8fb6a6700f93b5011e0
zhangeldy01 <shakhanov_zhan@mail.ru>
2025-03-07 14:38:49 +0300

    Added self-documentation

--- src/Products/HotDrink.java
+++ src/Products/HotDrink.java
@@ -1,10 +1,23 @@
 package Products;
 
+/**
+ * A subclass of a superclass Product
+ */
 public class HotDrink extends Product {
 
+    /**
+     * Field names
+     */
     private int temperature;
     private Double volume;
 
+    /**
+     * Class Constructor
+     * @param name product
+     * @param price product
+     * @param temperature product
+     * @param volume product
+     */
     public HotDrink(String name, Double price, int temperature, Double volume)
     {
         super(name, price);
@@ -13,22 +26,39 @@ public class HotDrink extends Product {
 
     }
 
+    /**
+     * get temperature
+     * @return temperature
+     */
     public int getTemperature() {
         return temperature;
     }
 
+    /**
+     * get volume
+     * @return volume
+     */
     public Double getVolume() {
         return volume;
     }
+
+    /**
+     * set a new temperature product
+     * @param temperature
+     */
     public void setTemperature(int temperature) {
         this.temperature = temperature;
     }
 
+    /**
+     * set a new volume product
+     * @param volume
+     */
     public void setVolume(Double volume) {
         this.volume = volume;
     }
     /**
-     * переопределение вывода продукта
+     * Override the product output
      */
     @Override
     public String toString()
